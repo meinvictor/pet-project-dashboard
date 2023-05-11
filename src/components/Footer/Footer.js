8cc9276e87bb1b465fad4a31f67f13d6c24b5abf
meinvictor <vi.mein37@gmail.com>
2023-05-11 20:59:25 +0300

    Initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// reactstrap components
+import { Container, Nav, NavItem, NavLink } from "reactstrap";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <Container fluid>
+        
+      </Container>
+    </footer>
+  );
+}
+
+export default Footer;
